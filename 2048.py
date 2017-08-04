e89aadc703f36e313988a462e85bf1633e19e8d9
Evan Keeton <2044295@edison.k12.nj.us>
2017-08-03 22:27:14 -0400

    Introduces index script - 2048.py
    Basic classes have been implemented and demonstrated, but much
    functionality - like looking updating available spots - remains
    to be seen.

--- 2048.py
+++ 2048.py
@@ -1,1 +1,78 @@
+#!/usr/bin/env python
 
+from board import textBoard
+import random
+
+#
+#  ______      __ _       _ _   _                 
+#  |  _  \    / _(_)     (_| | (_)                
+#  | | | |___| |_ _ _ __  _| |_ _  ___  _ __  ___ 
+#  | | | / _ |  _| | '_ \| | __| |/ _ \| '_ \/ __|
+#  | |/ |  __| | | | | | | | |_| | (_) | | | \__ \
+#  |___/ \___|_| |_|_| |_|_|\__|_|\___/|_| |_|___/
+#                                                 
+#                                                 
+
+class thisBoard(textBoard): # Slight modifications for what is necessary
+    def __init__(self, comp, width, height):
+        self.directions = [[-1, -1], [-1, 0], [-1, 1], [0, -1], 
+                           [0, 1], [1, -1], [1, 0], [1, 1]]
+        self.width = width
+        self.height = height
+          # no use for self.represent
+          # self.represent = []
+        self.spots = []
+        for x in range(width):
+            for y in range(height):
+                self.spots.append([x, y])
+        
+        for i in range(height):
+            self.append([])
+              # self.represent.append([])
+            for x in range(width):
+                self[i].append(comp)
+                  # self.represent[i].append(' ' + str(comp) + ' ')
+    
+    def change(self, lst, new):
+        self[lst[1]][lst[0]] = new
+
+    def showBoard(self):
+        rslt = '\n'
+        for x in self:
+            for y in range(3):
+                rslt += ' '.join([z[y] for z in x]) + '\n'
+            rslt += '\n'
+        return(rslt)
+
+class tile(list):
+    def __init__(self, num=''):
+        if num == '':
+            num = 0
+        self.num = int(num)
+        for x in range(3):
+            self.append('[     ]')
+        self.update()
+    
+    def update(self):
+        self[1] = '[{:^5}]'.format(self.num)
+        if self.num == 0:
+            self[1] = '[{:^5}]'.format('')
+        return(self)
+          
+    def numSet(self, num):
+        self.num = num
+
+#
+#   _____      _               
+#  /  ___|    | |              
+#  \ `--.  ___| |_ _   _ _ __  
+#   `--. \/ _ | __| | | | '_ \ 
+#  /\__/ |  __| |_| |_| | |_) |
+#  \____/ \___|\__|\__,_| .__/ 
+#                       | |    
+#                       |_|    
+
+board = thisBoard(tile(), 4, 4)
+print(board.showBoard())
+board.change(random.choice(board.spots), tile(2))
+print(board.showBoard())
