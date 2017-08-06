b29fc9abedfee7f987a3bcbf131da520d4f70d04
Evan Keeton <2044295@edison.k12.nj.us>
2017-08-06 14:37:18 -0400

    Add function for movement up, down, left, and right.
    Taking input and generating new tiles has not been included yet,
    but is the next step as this project begins to take shape.

--- 2048.py
+++ 2048.py
@@ -51,6 +51,47 @@ class thisBoard(board): # Slight modifications for what is necessary
                 rslt += ' '.join([z[y] for z in x]) + '\n'
             rslt += '\n'
         return(rslt)
+    
+    def doMove(self, udlr=None):
+        if udlr is None: return()
+        duplicate = [[x.num for x in y] for y in self]
+          # print(duplicate) # testing
+
+        if udlr == 'u':
+            duplicate = [[y[x] for y in duplicate] for x in range(self.height)]
+              # print(duplicate) # testing
+            duplicate = moveAll(duplicate)
+            for ind0, x in enumerate(duplicate):
+                for ind1, y in enumerate(x):
+                    self.change([ind0, ind1], tile(y))
+            return(duplicate)
+
+        elif udlr == 'd':
+            duplicate = [[y[x] for y in duplicate[::-1]] for x in range(self.height)]
+              # print(duplicate) # testing
+            duplicate = moveAll(duplicate)
+            for ind0, x in enumerate(duplicate):
+                for ind1, y in enumerate(x[::-1]):
+                    self.change([ind0, ind1], tile(y))
+            return(duplicate)
+
+        if udlr == 'l':
+            duplicate = [y[::-1] for y in duplicate]
+              # print(duplicate) # testing
+            duplicate = moveAll(duplicate)
+            for ind1, x in enumerate(duplicate):
+                for ind0, y in enumerate(x):
+                    self.change([3 - ind0, ind1], tile(y))
+            return(duplicate)
+
+        if udlr == 'r':
+            duplicate = [y for y in duplicate]
+            print(duplicate) # testing
+            duplicate = moveAll(duplicate)
+            for ind1, x in enumerate(duplicate):
+                for ind0, y in enumerate(x):
+                    self.change([ind0, ind1], tile(y))
+            return(duplicate)
 
 class tile(list):
     def __init__(self, num=''):
@@ -70,6 +111,16 @@ class tile(list):
     def numSet(self, num):
         self.num = num
 
+def moveAll(lst):
+    for row in lst:
+        for index in range(len(row)):
+            for num in range(index):
+                  # print(index, num, row) # testing
+                if row[num] == row[num + 1] or row[num + 1] == 0:
+                    row[num + 1] += row[num]; row[num] = 0
+                  # print() # testing
+    return(lst)
+
 #
 #   _____      _               
 #  /  ___|    | |              
@@ -86,3 +137,12 @@ for x in range(4):
     foo = random.choice(board.spots)
     board.change(foo, tile(2))
     print(board.showBoard())
+
+  # print(board.doMove('u')) # working
+  # print(board.showBoard())
+  # print(board.doMove('d')) # working
+  # print(board.showBoard())
+  # print(board.doMove('l')) # working
+  # print(board.showBoard())
+  # print(board.doMove('r')) # working
+  # print(board.showBoard())
